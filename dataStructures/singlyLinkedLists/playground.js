b264cbf22f5f0e4982f0f3e49763b8c3a70c3d61
Dylan <dylanfairbanks@icloud.com>
2020-06-01 12:21:41 -0600

    updated singlyLinkedList/playground.js

--- dataStructures/singlyLinkedLists/playground.js
+++ dataStructures/singlyLinkedLists/playground.js
@@ -1,13 +1,12 @@
 var linkedList = require('./singlyLinkedList');
 
 var myList = new linkedList;
+myList.push(1);
+myList.push(2);
 myList.push(3);
-myList.push('hello');
-myList.push(47);
-console.log(myList);
-myList.set(0, 'hamburger');
-myList.set(1, 'hamburger');
-myList.set(2, 'hamburger');
-console.log('*************')
-console.log(myList);
-console.log('*************')
+myList.push(4);
+myList.push(5);
+myList.print();
+console.log('***************')
+myList.reverse();
+myList.print();
