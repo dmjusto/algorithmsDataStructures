ec4f68b9e5200c9a3c2efeec43ecca2ee5c664c4
Dylan <dylanfairbanks@icloud.com>
2020-06-02 12:58:20 -0600

    implement depth first search and breadth first search on bst.js

--- dataStructures/queue/queue.js
+++ dataStructures/queue/queue.js
@@ -48,19 +48,4 @@ class Queue{
     }
 }
 
-var queue = new Queue();
-
-queue.enqueue(1);
-queue.print();
-queue.enqueue(2);
-queue.print();
-queue.enqueue(3);
-queue.print();
-queue.dequeue();
-queue.print();
-queue.dequeue();
-queue.print();
-queue.dequeue();
-queue.print();
-queue.dequeue();
-queue.print();
+module.exports = Queue;
--- dataStructures/trees/bst.js
+++ dataStructures/trees/bst.js
@@ -1,6 +1,8 @@
+const Queue = require('../queue/queue');
+
 class Node{
-    constructor(data){
-        this.data = data;
+    constructor(value){
+        this.value = value;
         this.leftChild = null;
         this.rightChild = null;
     }
@@ -11,8 +13,8 @@ class BinarySearchTree{
         this.root = null;
     }
 
-    insert(data){
-        let newNode = new Node(data);
+    insert(value){
+        let newNode = new Node(value);
 
         if(!this.root){
             this.root = newNode;
@@ -21,7 +23,7 @@ class BinarySearchTree{
 
         let currentNode = this.root;
         while(true){
-            if(data < currentNode.data){
+            if(value < currentNode.value){
                 if(!currentNode.leftChild){
                     currentNode.leftChild = newNode;
                     break;
@@ -29,7 +31,7 @@ class BinarySearchTree{
                 else currentNode = currentNode.leftChild;
                 
             }
-            else if(data > currentNode.data){
+            else if(value > currentNode.value){
                 if(!currentNode.rightChild){
                     currentNode.rightChild = newNode;
                     break;
@@ -41,31 +43,104 @@ class BinarySearchTree{
         return this;
     }
 
-    find(data){
-        if(!this.root || !data) return false;
+    find(value){
+        if(!this.root || !value) return false;
 
         let currentNode = this.root;
         while(currentNode){
-            if(data < currentNode.data){
+            if(value < currentNode.value){
                 currentNode = currentNode.leftChild;
             }
-            else if(data > currentNode.data){
+            else if(value > currentNode.value){
                 currentNode = currentNode.rightChild;
             }else return true;
         }
         return false;
     }
+
+    //BREADTH FIRST SEARCH
+    bfs(){
+        if(!this.root) return null;
+
+        let searchQueue = new Queue();
+        let result = [];
+        searchQueue.enqueue(this.root);
+
+        while(searchQueue.size > 0){
+            const current = searchQueue.dequeue();
+            result.push(current.value);
+
+            if(current.leftChild) searchQueue.enqueue(current.leftChild);
+            if(current.rightChild) searchQueue.enqueue(current.rightChild);
+        }
+        return result;
+    }
+
+    // DEPTH FIRST SEARCH
+    dfsPreOrder(){
+        if(!this.root) return null;
+
+        let data = [];
+        traverse(this.root);
+
+        return data;
+
+        function traverse(node){
+            data.push(node.value);
+            if(node.leftChild) traverse(node.leftChild);
+            if(node.rightChild) traverse(node.rightChild);
+        }
+    }
+
+    dfsPostOrder(){
+        if(!this.root) return null;
+
+        let data = [];
+        Traverse(this.root);
+
+        return data;
+
+        function Traverse(node){
+            if(node.leftChild) Traverse(node.leftChild);
+            if(node.rightChild) Traverse(node.rightChild);
+            data.push(node.value);
+        }
+    }
+
+    dfsInOrder(){
+        if(!this.root) return null;
+
+        let data = [];
+        Traverse(this.root);
+
+        return data;
+
+        function Traverse(node){
+            if(node.leftChild) Traverse(node.leftChild);
+            data.push(node.value);
+            if(node.rightChild) Traverse(node.rightChild);
+        }
+    }
 }
 
+
+//         50
+//      12       67
+//    7      51       99
+//  3  9       60  73   106
 var tree = new BinarySearchTree();
 tree.insert(50);
 tree.insert(12);
 tree.insert(67);
 tree.insert(7);
-console.log(tree);
-
-console.log(tree.find(50));
-console.log(tree.find(12));
-console.log(tree.find(67));
-console.log(tree.find(7));
-console.log(tree.find(99));
+tree.insert(99);
+tree.insert(3);
+tree.insert(51);
+tree.insert(9);
+tree.insert(60);
+tree.insert(73);
+tree.insert(106);
+console.log(tree.bfs());
+console.log(tree.dfsPreOrder());
+console.log(tree.dfsPostOrder());
+console.log(tree.dfsInOrder());
